546d4bf026a9f462479b65a843cfb30a66903d8b
WTFender <wtfender.cs@gmail.com>
2021-06-18 11:26:58 -0400

    refactor

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM wpscanteam/wpscan
 COPY entrypoint.sh /entrypoint.sh
+USER root
+RUN apk add curl
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo ::set-output name=result::$time
+/usr/local/bundle/wpscan --update
+
+curl $1
+
+/usr/local/bundle/wpscan --disable-tls-checks --url $1 --api-token $2 -f json -o result.json
+
+RESULT=$(cat result.json | base64)
+
+echo ::set-output name=result::$RESULT
