3709685ff5f790490b3a099221bd68a4acfd1377
WTFender <wtfender.cs@gmail.com>
2021-06-18 16:36:43 -0400

    add curl

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 /usr/local/bundle/bin/wpscan --update
+
+curl -s $1
+
 /usr/local/bundle/bin/wpscan --disable-tls-checks --url $1 --api-token $2 -f json -o result.json
 
 RESULT=$(cat result.json | base64)
