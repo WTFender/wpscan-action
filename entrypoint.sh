e9aabf31ff0bb42ead69d8b0a8fb3725d9e0e760
WTFender <wtfender.cs@gmail.com>
2021-06-18 17:03:57 -0400

    add options

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
+
+# $1 URL
+# $2 TOKEN
+# $3 OPTIONS
+
+ARGS="--disable-tls-checks -f json --url ${1}"
+[  -z "$2" ] && ARGS="${ARGS} --api-token=${2}"
+[  -z "$3" ] && ARGS="${ARGS} ${3}"
+
 WPSCAN=/usr/local/bundle/bin/wpscan
 
 $WPSCAN --update
 
-RESULT=$($WPSCAN --disable-tls-checks --url $1 --api-token $2 -f json)
-
+RESULT=$($WPSCAN $ARGS)
 RESULT_B64=$(echo $RESULT | base64)
 
 echo ::set-output name=result::$RESULT
