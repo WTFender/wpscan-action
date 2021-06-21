d372cd6bc0b45a54560e38d5b8b154817ac1a1c2
WTFender <wtfender.cs@gmail.com>
2021-06-21 17:47:33 -0400

    test

--- webhook.py
+++ webhook.py
@@ -7,16 +7,35 @@ RESULTB64 = sys.argv[1]
 WEBHOOK = sys.argv[2]
 WEBHOOKOPTS = sys.argv[3]
 
-RESULT = base64.b64decode(RESULTB64).decode('utf-8')
+RESULT = json.loads(base64.b64decode(RESULTB64).decode('utf-8'))
 
-if WEBHOOK:
-    x = json.loads(RESULT)
-    payload = {
-        'text': 'testsetsetseting!'
-    }
 
-    r = requests.post(WEBHOOK, json=payload)
-    print(r.status_code)
+def scan():
+    return {'text': 'Scan completed'}
 
-else:
-    print('No webhook')
+def abort():
+    return {'text': 'Scan aborted'}
+
+def vulns():
+    return {'text': 'Vulns found'}
+
+
+
+if not WEBHOOK:
+    exit('No webhook')   
+    
+if 'scan_aborted' in RESULT:
+    requests.post(WEBHOOK, json=abort())
+    exit('Scan aborted')
+
+VULNS = ( len(RESULT['version']['vulnerabilities']) +
+        len(RESULT['main_theme']['vulnerabilities']) + 
+        sum([len(RESULT['plugins'][p]['vulnerabilities']) for p in RESULT['plugins']]) )
+
+if VULNS:
+    requests.post(WEBHOOK, json=vulns())
+    exit('Vulns found')
+
+if WEBHOOKOPTS:
+    requests.post(WEBHOOK, json=scan())
+    exit('Scan completed')
