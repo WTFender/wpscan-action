093f947d74459ba6b5a962da7470656317e6b78e
WTFender <wtfender.cs@gmail.com>
2021-06-21 16:45:51 -0400

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,17 @@
 # $3 OPTIONS
 # $4 WEBHOOK
 
-ARGS="-f json --url ${1}"
-[  -n "$2" ] && ARGS="${ARGS} --api-token=${2}"
-[  -n "$3" ] && ARGS="${ARGS} ${3}"
+#ARGS="-f json --url ${1}"
+#[  -n "$2" ] && ARGS="${ARGS} --api-token=${2}"
+#[  -n "$3" ] && ARGS="${ARGS} ${3}"
+#
+#WPSCAN=/usr/local/bundle/bin/wpscan
+#
+#$WPSCAN --update
 
-WPSCAN=/usr/local/bundle/bin/wpscan
 
-$WPSCAN --update
-
-RESULT=$($WPSCAN $ARGS)
+#RESULT=$($WPSCAN $ARGS)
+RESULT=$(cat /example.json)
 RESULT_B64=$(echo $RESULT | base64)
 
 echo ::set-output name=result::$RESULT
