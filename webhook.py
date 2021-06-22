b4abf54448ba49463c450365843e781668f43383
WTFender <wtfender.cs@gmail.com>
2021-06-22 17:20:12 -0400

    format webhook

--- webhook.py
+++ webhook.py
@@ -12,15 +12,61 @@ RESULT = json.loads(base64.b64decode(RESULTB64).decode('utf-8'))
 
 def scan():
     print('Scan completed')
-    return {'text': 'Scan completed'}
+    return {'text': 'Scan completed', 'color': 'success'}
     
 def abort():
     print('Scan aborted')
-    return {'text': 'Scan aborted'}
+    return {'text': 'Scan aborted', 'color': 'warning'}
 
 def vulns(n):
     print('Vulns found')
-    return {'text': '%s vulns found' % n}
+    attachments = []
+
+    #for v in RESULT['version']['vulnerabilities']:
+    #    pass
+    #
+    #for v in RESULT['main_theme']['vulnerabilities']:
+    #    pass
+    
+    for p in RESULT['plugins']:
+        for v in RESULT['plugins'][p]['vulnerabilities']:
+            version = f"{RESULT['plugins'][p]['version']['number']} ({RESULT['plugins'][p]['version']['confidence']}%)"
+            vuln = {
+                "color": "danger",
+                "fallback": v['title'],
+                "title": v['title'],
+                "fields": [
+                    {
+                        'title': 'Version',
+                        'value': version,
+                        'short': True
+                    }
+                ]
+            }
+            
+            if 'url' in v['references']:
+                vuln['title_link'] = v['references']['url'][0]
+
+            if 'cve' in v['references']:
+                cve = 'CVE-'+v['references']['cve'][0]
+                vuln['author_name'] = cve
+                vuln['author_link'] = 'https://cve.mitre.org/cgi-bin/cvename.cgi?name='+cve
+
+            if 'fixed_in' in v:
+                vuln['fields'].append(
+                    {
+                        'title': 'Fixed Version',
+                        'value': v['fixed_in'],
+                        'short': True
+                    }
+                )
+            attachments.append(vuln)
+
+    return {
+        'username': 'WPScan',
+        'text': 'Vulnerabilities found',
+        'attachments': attachments
+    }
     
 
 if 'scan_aborted' in RESULT:
@@ -36,4 +82,3 @@ if VULNS:
 if WEBHOOK:
     r = requests.post(WEBHOOK, json=payload)
     print(f'Webhook: %s' % r.status_code)
-    
